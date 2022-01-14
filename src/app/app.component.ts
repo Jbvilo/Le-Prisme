517fcfbc12433df944e65fe317ba8954b3a4ab45
DESKTOP-5TA0IDD\jbvil <jbvilo@hotmail.com>
2022-01-15 00:27:23 +0100

    Welcome animation 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'LePrismeWeb';
+export class AppComponent implements OnInit {
+  title = 'Le Prisme';
+  visible: boolean = false;
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.visible = true
+    }, 6000);
+  }
 }
