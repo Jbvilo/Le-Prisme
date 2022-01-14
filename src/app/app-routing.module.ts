ab854aece2687a9b42ca9454f2b439e688d97cbf
DESKTOP-5TA0IDD\jbvil <jbvilo@hotmail.com>
2022-01-14 19:48:05 +0100

    Test du routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { PresentationComponent } from './pages/presentation/presentation.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"", component:HomeComponent},
+  {path:"presentation", component:PresentationComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ServicesComponent } from './pages/services/services.component';
+import { PresentationComponent } from './pages/presentation/presentation.component';
+import { PartenairesComponent } from './pages/partenaires/partenaires.component';
+import { ContactsComponent } from './pages/contacts/contacts.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    ServicesComponent,
+    PresentationComponent,
+    PartenairesComponent,
+    ContactsComponent
   ],
   imports: [
     BrowserModule,
