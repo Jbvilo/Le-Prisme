c027ce3161b0510887d3acbe25c7d71be545d58a
DESKTOP-5TA0IDD\jbvil <jbvilo@hotmail.com>
2022-01-16 06:52:13 +0100

    no message

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { PartenairesComponent } from './pages/partenaires/partenaires.component';
 import { PresentationComponent } from './pages/presentation/presentation.component';
 import { ServicesComponent } from './pages/services/services.component';
 import { WelcomeComponent } from './pages/welcome/welcome.component';
@@ -9,7 +10,8 @@ const routes: Routes = [
   {path:"", component:WelcomeComponent},
   {path:"home", component:HomeComponent},
   {path:"presentation", component:PresentationComponent},
-  {path:"services", component:ServicesComponent}
+  {path:"services", component:ServicesComponent},
+  {path:"partenaires", component:PartenairesComponent}
 ];
 
 @NgModule({
