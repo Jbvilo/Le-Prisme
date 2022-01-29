2a5a037da6eebe939d8639b6c29e2f0ec86ecc0d
DESKTOP-5TA0IDD\jbvil <jbvilo@hotmail.com>
2022-01-29 22:08:36 +0100

    no message

--- src/app/components/home/firstview/firstview.component.ts
+++ src/app/components/home/firstview/firstview.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs/internal/Observable';
+
+
 
 @Component({
   selector: 'app-firstview',
@@ -7,13 +10,29 @@ import { Router } from '@angular/router';
   styleUrls: ['./firstview.component.scss']
 })
 export class FirstviewComponent implements OnInit {
+  eventSubscription: any;
 
-  constructor(private router:Router) { }
+  constructor(private router:Router) {
+    
+   }
+   @HostListener('window:scroll', ['$event'])
+   onWindowScroll($event) {
+       console.log($event);
+   }
+
 
   ngOnInit(): void {
+
+    
   }
   navigateTo(path) {
+    if(path =="chatbox.html"){
+  location.href =path
+    }
 this.router.navigate([path])
+  }
+  onScroll(event){
+    alert(event)
   }
 
 }
