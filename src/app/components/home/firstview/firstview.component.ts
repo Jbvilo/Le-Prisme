6a0aec2b95099048cd07ac834869e7970046480c
DESKTOP-5TA0IDD\jbvil <jbvilo@hotmail.com>
2022-02-04 16:09:01 +0100

    screen size

--- src/app/components/home/firstview/firstview.component.ts
+++ src/app/components/home/firstview/firstview.component.ts
@@ -18,6 +18,7 @@ export class FirstviewComponent implements OnInit {
 
 
   ngOnInit(): void {
+    this.screensize()
     window.addEventListener('touchstart', function (e) {
       if (e.touches.length > 1) {
         alert()
@@ -34,11 +35,20 @@ export class FirstviewComponent implements OnInit {
   navigate(anchor?) {
     let i = 0;
     let interval = setInterval(() => {
-      if (i < 800) {
+      if (i < this.screensize()) {
         window.scroll({ left: 0, top: i = i + 5 })
       } else {
         clearInterval(interval)
       }
     }, 5);
+  }
+
+  screensize():number{
+    if(window.screen.height <= 720){
+    return 520;
+    }
+    if(window.screen.height <= 1080){
+      return 800
+    }
   }
 }
