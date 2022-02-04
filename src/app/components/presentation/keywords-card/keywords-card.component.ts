da55da9788445153d5673a706bca079d054d1926
DESKTOP-5TA0IDD\jbvil <jbvilo@hotmail.com>
2022-02-04 16:37:06 +0100

    no message

--- src/app/components/presentation/keywords-card/keywords-card.component.ts
+++ src/app/components/presentation/keywords-card/keywords-card.component.ts
@@ -16,7 +16,8 @@ visible:boolean=false
    }
   @HostListener('window:scroll', ['$event'])
   onWindowScroll($event) {
-    if(window.scrollY > 700){
+    if(window.screen.height<=720){
+    if(window.scrollY > 1230){
     let interval=setInterval(() => {
       if(this.chantier<300){
         this.chantier++
@@ -32,6 +33,24 @@ visible:boolean=false
       }
     },100);
     }
+  }
+  else {
+    if(window.scrollY > 1650){
+      let interval=setInterval(() => {
+        if(this.chantier<300){
+          this.chantier++
+        }
+        if(this.exp<30){
+          this.exp++;
+        }
+        if(this.chantier >= 300){
+          clearInterval(interval)
+          this.visible=true
+        }
+      },100);
+      }
+
+  }
   }
 
   ngOnInit(): void {
