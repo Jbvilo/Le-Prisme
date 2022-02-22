c9305565b3511916cb686dbb8f0713736f95c68f
DESKTOP-5TA0IDD\jbvil <jbvilo@hotmail.com>
2022-02-23 00:12:55 +0100

    no message

--- src/app/pages/welcome/welcome.component.ts
+++ src/app/pages/welcome/welcome.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -7,14 +7,12 @@ import { Router } from '@angular/router';
   styleUrls: ['./welcome.component.scss']
 })
 export class WelcomeComponent implements OnInit {
-  constructor(private router:Router) { }
-
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
     setTimeout(() => {
       this.router.navigate(['home'])
     }, 4500);
- 
   }
 
 }
