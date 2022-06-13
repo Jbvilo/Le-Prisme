289f43793b2e742ef62f0e2e58baa824c9a08c5d
DESKTOP-5TA0IDD\jbvil <jbvilo@hotmail.com>
2022-06-14 00:32:26 +0200

    no message

--- src/app/components/navigation-bar/navigation-bar.component.ts
+++ src/app/components/navigation-bar/navigation-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -8,16 +8,30 @@ import { Router } from '@angular/router';
 })
 export class NavigationBarComponent implements OnInit {
   current_path: string;
+  mobile :boolean = false;
   constructor(private route: Router) {
     this.current_path = this.route.url
     if (this.current_path == "/hellodjo" || this.current_path == "/aah" || this.current_path == "/renovation") {
       this.current_path = "/services"
     }
+    
   }
 
-
+  @HostListener('window:resize', ['$event'])
+onResize(event) {
+if(window.innerWidth <= 500){
+  this.mobile = true
+}
+else {
+  this.mobile = false
+}
+}
   ngOnInit(): void {
     this.router_subscribe()
+    console.log(window.innerWidth)
+    if (window.innerWidth<= 500){
+      this.mobile = true;
+    }
   }
 
   router_subscribe(): void {
