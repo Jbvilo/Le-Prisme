a07134e8a369090685826dcd382740d8bb722d1c
DESKTOP-5TA0IDD\jbvil <jbvilo@hotmail.com>
2022-06-16 21:07:15 +0200

    no message

--- src/app/components/services/services-presentation/services-presentation.component.ts
+++ src/app/components/services/services-presentation/services-presentation.component.ts
@@ -16,4 +16,25 @@ export class ServicesPresentationComponent implements OnInit {
   navigateTo(path): void {
 
   }
+
+  
+  scroll(anchor?) {
+    let i = 0;
+    let interval = setInterval(() => {
+      if (i < this.screensize()) {
+        window.scroll({ left: 0, top: i = i + 5 })
+      } else {
+        clearInterval(interval)
+      }
+    }, 5);
+  }
+
+  screensize(): number {
+    if (window.screen.height <= 720) {
+      return 520;
+    }
+    if (window.screen.height <= 1080) {
+      return 800
+    }
+  }
 }
