937d046c3a5c94bd83dc4ecd748ce6157ce72bc2
DESKTOP-5TA0IDD\jbvil <jbvilo@hotmail.com>
2022-07-30 00:51:28 +0200

    no message

--- src/app/components/home/avantages/avantages.component.ts
+++ src/app/components/home/avantages/avantages.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -7,12 +7,25 @@ import { Router } from '@angular/router';
   styleUrls: ['./avantages.component.scss']
 })
 export class AvantagesComponent implements OnInit {
+  mobile: boolean;
 
   constructor(private route:Router) { }
 
   ngOnInit(): void {
+    if (window.innerWidth<= 500){
+      this.mobile = true;
+    }
+   }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+  if(window.innerWidth <= 500){
+    this.mobile = true
   }
-  
+  else {
+    this.mobile = false
+  }
+  }
   navigateTo(path): void {
     this.route.navigate([path])
   }
