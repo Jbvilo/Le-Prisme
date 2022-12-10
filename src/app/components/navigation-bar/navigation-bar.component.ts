9e800c52a09ee5e6fa0e9a6cf704fc5114ef41ba
DESKTOP-5TA0IDD\jbvil <jbvilo@hotmail.com>
2022-12-10 04:24:48 +0100

    no message

--- src/app/components/navigation-bar/navigation-bar.component.ts
+++ src/app/components/navigation-bar/navigation-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -7,12 +7,26 @@ import { Router } from '@angular/router';
   styleUrls: ['./navigation-bar.component.scss']
 })
 export class NavigationBarComponent implements OnInit {
+  mobile: boolean;
 
   constructor(private route: Router) { }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    if (window.innerWidth <= 500) {
+      this.mobile = true;
+    }
+  }
 
   navigateTo(path): void {
     this.route.navigate([path])
+  }
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    if (window.innerWidth <= 500) {
+      this.mobile = true
+    }
+    else {
+      this.mobile = false
+    }
   }
 }
