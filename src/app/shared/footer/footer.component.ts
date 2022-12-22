6f417b5f80e894abcb86f4183c9f0910fbb82de2
DESKTOP-5TA0IDD\jbvil <jbvilo@hotmail.com>
2022-12-22 20:23:37 +0100

    no message

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router : Router) { }
 
   ngOnInit(): void {
+  }
+  iscontact(){
+    if(this.router.url == '/contacts'){
+return 'gradiant-1'
+    }
+    else {
+      return 'black'
+    }
   }
 
 }
