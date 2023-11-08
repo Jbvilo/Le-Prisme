72b5a1ee5bf853a7ae07f69fdd364a507d77cf7e
DESKTOP-5TA0IDD\jbvil <jbvilo@hotmail.com>
2023-11-08 02:34:22 +0100

    try

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './views/home/home.component';
+import { RequestsComponent } from './views/requests/requests.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
+  { path: 'request', component: RequestsComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' }
 ];
 
